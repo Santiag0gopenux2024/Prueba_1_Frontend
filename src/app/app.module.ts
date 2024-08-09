bd41bd2768ef893a28dc54415798fe757969d8c3
Santiag0gopenux2024 <michael.martinez@gopenux.com>
2024-08-09 08:28:27 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Section1Component } from './components/section1/section1.component';
+import { Section2Component } from './components/section2/section2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Section1Component,
+    Section2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
